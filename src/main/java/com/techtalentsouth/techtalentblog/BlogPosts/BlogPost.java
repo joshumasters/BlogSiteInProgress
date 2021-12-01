8f4a41f3d63957d1886e47240e8301ecc2d9cb05
Joshua Masters <joshmasters724@gmail.com>
2021-12-01 13:58:39 -0500

    Added Delete page, styling, navbar

--- src/main/java/com/techtalentsouth/techtalentblog/BlogPosts/BlogPost.java
+++ src/main/java/com/techtalentsouth/techtalentblog/BlogPosts/BlogPost.java
@@ -22,6 +22,14 @@ public class BlogPost {
         this.author = author;
         this.blogEntry = blogEntry;
     }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
